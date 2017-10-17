03d2034f425c23a76c75687aadab89291417b077
Victor Lava <hello@victorlava.com>
2017-10-17 18:25:05 +0300

    added grunt production task, which runs the sass files, minifies css and js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,12 +37,39 @@ module.exports = function(grunt) {
       } //Add Live Reload Chrome Extension for this to work
     },
 
+    uglify: {
+      all: {
+        files: [{
+          expand: true,
+          cwd: 'js/',
+          src: ['*.js'],
+          dest: 'www/js',
+          ext: '.min.js',
+        }],
+      },
+    },
+    
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'www/css',
+          src: ['*.css'],
+          dest: 'www/css',
+          ext: '.min.css'
+        }]
+      }
+    }
+
 
   });
 
 grunt.loadNpmTasks('grunt-sass');
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-serve');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 grunt.registerTask('default', 'watch');
+grunt.registerTask('production', ['sass', 'cssmin', 'uglify']);
 };
